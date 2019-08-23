b7a8896e9bb2a95e6d2a19aa4b16ef1539ca7d1b
Martin Spusta <martin.spusta@gmail.com>
2019-08-23 14:43:46 -0400

    Covert to MVC design patterns

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Martin Spusta on 8/23/19.
+//  Copyright © 2019 Itkido. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done: Bool = false
+    
+    
+}
